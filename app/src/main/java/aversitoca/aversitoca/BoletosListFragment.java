db8da85abb3a7c65d54ad3c683f02ede30b864a6
Marcos Muñoz Nuñez <marcos.munoz.nunez@uva.es>
2018-01-08 22:50:42 +0100

    Fragment Lista Boletos

---
+++ app/src/main/java/aversitoca/aversitoca/BoletosListFragment.java
@@ -0,0 +1,18 @@
+package aversitoca.aversitoca;
+
+import android.app.Fragment;
+import android.app.ListFragment;
+import android.widget.SimpleCursorAdapter;
+
+/**
+ * Created by Marcos Muñoz on 04/01/2018.
+ */
+
+public class BoletosListFragment extends ListFragment {
+
+    private SimpleCursorAdapter myAdapter;
+
+    //TODO: Especificar columnas a mapear...
+
+
+}
