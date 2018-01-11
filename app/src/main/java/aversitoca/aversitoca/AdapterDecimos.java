5836435fc051c306d50eec3153d350305ffca6b7
Marcos Muñoz Nuñez <marcos.munoz.nunez@alumnos.uva.es>
2018-01-11 01:01:00 +0100

    Swipe to delete (Semi-Running)

--- app/src/main/java/aversitoca/aversitoca/AdapterDecimos.java
+++ app/src/main/java/aversitoca/aversitoca/AdapterDecimos.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -25,7 +26,7 @@ public class AdapterDecimos extends RecyclerView.Adapter<AdapterDecimos.ViewHold
     @Override
     public ViewHolderDecimos onCreateViewHolder(ViewGroup parent, int viewType) {
         View view= LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.list_item_decimo,null,false);
+                .inflate(R.layout.list_item_decimo2,null,false);
 
         return new ViewHolderDecimos(view);
     }
@@ -34,7 +35,7 @@ public class AdapterDecimos extends RecyclerView.Adapter<AdapterDecimos.ViewHold
     public void onBindViewHolder(ViewHolderDecimos holder, int position) {
         holder.numeroDecimo.setText(listDecimos.get(position).getNumero().toString());
         holder.nombreSorteo.setText(listDecimos.get(position).getSorteo());
-        holder.premio.setText(listDecimos.get(position).getPremio().toString());
+        holder.premio.setText(listDecimos.get(position).getPremio().toString() + "€");
        //TODO: Insertar foto en -
         // holder.foto.setImageResoruce(listDecimos.get(position).getFoto());
     }
@@ -44,22 +45,40 @@ public class AdapterDecimos extends RecyclerView.Adapter<AdapterDecimos.ViewHold
         return listDecimos.size();
     }
 
+    public void removeItem(int position) {
+        listDecimos.remove(position);
+        // notify the item removed by position
+        // to perform recycler view delete animations
+        // NOTE: don't call notifyDataSetChanged()
+        notifyItemRemoved(position);
+    }
+
+    public void restoreItem(Decimo item, int position) {
+        listDecimos.add(position, item);
+        // notify item added by position
+        notifyItemInserted(position);
+    }
+
+
     public class ViewHolderDecimos extends RecyclerView.ViewHolder {
 
         TextView numeroDecimo;
         TextView nombreSorteo;
         TextView premio;
         ImageView foto;
+        public RelativeLayout viewBackground, viewForeground;
 
 
 
 
         public ViewHolderDecimos(View itemView) {
             super(itemView);
-            numeroDecimo=(TextView) itemView.findViewById(R.id.text_numero_boleto);
-            nombreSorteo=(TextView)itemView.findViewById(R.id.nombre_sorteo);
-            premio=(TextView) itemView.findViewById(R.id.second_price_text_view);
-            foto=(ImageView) itemView.findViewById(R.id.foto_decimo);
+            numeroDecimo=(TextView) itemView.findViewById(R.id.numero2);
+            nombreSorteo=(TextView)itemView.findViewById(R.id.sorteo2);
+            premio=(TextView) itemView.findViewById(R.id.premio2);
+            foto=(ImageView) itemView.findViewById(R.id.foto2);
+            viewBackground = itemView.findViewById(R.id.view_background);
+            viewForeground = itemView.findViewById(R.id.view_foreground);
         }
 
     }
--- app/src/main/java/aversitoca/aversitoca/Main2Activity.java
+++ app/src/main/java/aversitoca/aversitoca/Main2Activity.java
@@ -1,10 +1,17 @@
 package aversitoca.aversitoca;
 
 import android.database.Cursor;
+import android.graphics.Color;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -12,6 +19,8 @@ public class Main2Activity extends AppCompatActivity {
 
     ArrayList<Decimo> listDecimos;
     RecyclerView recyclerDecimos;
+     CoordinatorLayout coordinatorLayout;
+    private AdapterDecimos mAdapter;
 
 
 
@@ -23,7 +32,14 @@ public class Main2Activity extends AppCompatActivity {
 
         listDecimos=new ArrayList<>();
         recyclerDecimos= (RecyclerView) findViewById(R.id.recyclerId);
+        coordinatorLayout = findViewById(R.id.coordinator_layout);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerDecimos.setLayoutManager(new LinearLayoutManager(this));
+        recyclerDecimos.setLayoutManager(mLayoutManager);
+        recyclerDecimos.setItemAnimator(new DefaultItemAnimator());
+        recyclerDecimos.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
 
         consultarListaDecimos();
 
@@ -31,8 +47,56 @@ public class Main2Activity extends AppCompatActivity {
         AdapterDecimos adapter=new AdapterDecimos((listDecimos));
         recyclerDecimos.setAdapter(adapter);
 
+        // adding item touch helper
+        // only ItemTouchHelper.LEFT added to detect Right to Left swipe
+        // if you want both Right -> Left and Left -> Right
+        // add pass ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT as param
+
+        //ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new RecyclerItemTouchHelper(0, ItemTouchHelper.LEFT, this);
+        ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new RecyclerItemTouchHelper(0, ItemTouchHelper.LEFT);
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerDecimos);
+
+
 
     }
+
+
+
+    /**
+     * callback when recycler view is swiped
+     * item will be removed on swiped
+     * undo option will be provided in snackbar to restore the item
+     */
+
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
+        if (viewHolder instanceof AdapterDecimos.ViewHolderDecimos) {
+            // get the removed item name to display it in snack bar
+            String name = listDecimos.get(viewHolder.getAdapterPosition()).getPremio();
+
+            // backup of removed item for undo purpose
+            final Decimo deletedItem = listDecimos.get(viewHolder.getAdapterPosition());
+            final int deletedIndex = viewHolder.getAdapterPosition();
+
+            // remove the item from recycler view
+            mAdapter.removeItem(viewHolder.getAdapterPosition());
+
+            // showing snack bar with Undo option
+            Snackbar snackbar = Snackbar
+                    .make(coordinatorLayout, name + " removed from cart!", Snackbar.LENGTH_LONG);
+            snackbar.setAction("UNDO", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    // undo is selected, restore the deleted item
+                    mAdapter.restoreItem(deletedItem, deletedIndex);
+                }
+            });
+            snackbar.setActionTextColor(Color.YELLOW);
+            snackbar.show();
+        }
+    }
+
+
 
     private void consultarListaDecimos() {
         Decimo decimo =null;
@@ -44,7 +108,7 @@ public class Main2Activity extends AppCompatActivity {
             decimo.setId(cursor.getInt(0));
             decimo.setNumero(cursor.getString(1));
             decimo.setPremio(cursor.getString(2));
-            decimo.setPremio(cursor.getString(3));
+            decimo.setSorteo(cursor.getString(3));
             decimo.setFoto(cursor.getInt(0));
 
         listDecimos.add(decimo);
