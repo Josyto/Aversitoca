3c89a5f11e2b806882024454170cecf5611055c3
Josyto <josyto7@gmail.com>
2018-01-12 16:54:14 +0100

    CAMBIOS EN NETWATCHER

--- app/src/main/java/aversitoca/aversitoca/NetWatcher.java
+++ app/src/main/java/aversitoca/aversitoca/NetWatcher.java
@@ -1,26 +1,23 @@
 package aversitoca.aversitoca;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkInfo;
-import android.os.Build;
-import android.util.Log;
 import android.widget.Toast;
 
 
 
 public class NetWatcher extends BroadcastReceiver {
 
-    private static final String LOG_TAG = "NetworkChangeReceiver";
-    private boolean isConnected = false;
-
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     @Override
     public void onReceive(Context context, Intent intent) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo info = cm.getActiveNetworkInfo();
+        NetworkInfo info = cm != null ? cm.getActiveNetworkInfo() : null;
         isNetworkAvailable(context);
         if (info != null) {
             if (info.isConnected()) {
@@ -36,33 +33,17 @@ public class NetWatcher extends BroadcastReceiver {
         }
     }
 
-    private boolean isNetworkAvailable(Context context) {
+    private void isNetworkAvailable(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Network[] networks = connectivityManager.getAllNetworks();
-            NetworkInfo networkInfo;
-            for (Network mNetwork : networks) {
-                networkInfo = connectivityManager.getNetworkInfo(mNetwork);
-                if (networkInfo.getState().equals(NetworkInfo.State.CONNECTED)) {
-                    return true;
-                }
-            }
-        }else {
-            if (connectivityManager != null) {
-                //noinspection deprecation
-                NetworkInfo[] info = connectivityManager.getAllNetworkInfo();
-                if (info != null) {
-                    for (NetworkInfo anInfo : info) {
-                        if (anInfo.getState() == NetworkInfo.State.CONNECTED) {
-                            Log.d("Network",
-                                    "NETWORKNAME: " + anInfo.getTypeName());
-                            return true;
-                        }
-                    }
-                }
+        assert connectivityManager != null;
+        Network[] networks = connectivityManager.getAllNetworks();
+        NetworkInfo networkInfo;
+        for (Network mNetwork : networks) {
+            networkInfo = connectivityManager.getNetworkInfo(mNetwork);
+            if (networkInfo.getState().equals(NetworkInfo.State.CONNECTED)) {
+                return;
             }
         }
         Toast.makeText(context,context.getString(R.string.connect),Toast.LENGTH_SHORT).show();
-        return false;
     }
 }
